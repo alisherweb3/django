f493c164d5ddf076d8b14427e40236061291c938
alisherweb3 <42702073+alisherweb3@users.noreply.github.com>
2022-10-20 15:37:29 +0500

    Update rubric.py

--- rubric.py
+++ rubric.py
@@ -1,6 +1,8 @@
 class Rubric(models.Model):
   name = models.CharField(max_length=20, db_index=True, verbose_name='Name')
   
+  rubric = models.ForeignKey('Rubric', null=True, on_delete=models.PROTECT, verbose_name='RUBRICA')
+  
   class Meta:
     verbose_name_plural = 'Rubrics'
     verbose_name = 'Rubrica'
